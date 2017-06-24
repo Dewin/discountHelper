b0049e3b32901ff1bdb1f3e708a9bfbba9ab912f
Dewin <simon@temtron.se>
2017-06-25 01:02:48 +0200

    testplace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.List;
+
 /**
  * Created by Simon on 2017-06-24.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("tjena");
+    public static void main(String[] args) throws Exception {
+        StoreScanner storeScanner = new StoreScanner();
+        List<Store> stores = storeScanner.getStoresInRadius(2000);
+        for (Store store : stores) {
+            System.out.println(store.getName());
+            System.out.println("DISTANCE FROM YOU: " + store.getDistanceToUser());
+        }
     }
 }
